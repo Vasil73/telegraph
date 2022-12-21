2a92bdb9ec91bec49a999fcec07c166edaa9fdb9
vasily <vs300259@gmail.com>
2022-12-21 14:44:01 +0300

    telegraph/

--- TelegraphText.php
+++ TelegraphText.php
@@ -4,7 +4,7 @@ require_once 'index.php';
 
 
 class TelegraphText {
-    public $text, $title, $author, $published, $slug, $fileStorage;
+    private $text, $title, $author, $published, $slug, $fileStorage;
 
 
     public function __construct(string $author, string $published, string $slug, string $fileStorage)
@@ -15,14 +15,60 @@ class TelegraphText {
         $this->slug = $slug;
     }
 
-    public function storeText(): void
+    public function __set(string $name, string $value)
+    {
+        switch ($name) {
+            case 'author' :
+                if (strlen($value) > 120) {
+                    echo 'Длина строки не может привышать 120 символов' . PHP_EOL;
+                    return;
+                }
+                $this->author = $value;
+                break;
+
+            case 'slug' :
+                $pattern = '/[^A-Za-z0-9-_\/]/';
+                if (preg_match($pattern, $value)) {
+                    echo 'Ошибка! Поле slug может содержать только буквы латинского
+                     алфавита, цифры, и символы (тире, нижнее подчеркивание, слэш)' . PHP_EOL;
+                    return;
+                }
+                $this->slug = $value;
+                break;
+            case 'published' :
+                if (strtotime($value) < date('Y-m-d')) {
+                    echo 'Ошибка! Дата должна быть больше или равна текущей';
+                    return;
+                }
+                $this->published = $value;
+                break;
+            case 'text' :
+                $this->text = $value;
+                $this->storeText();
+                break;
+            default:
+                return;
+        }
+
+    }
+
+    public function __get($name){
+
+        if ($name === 'text') {
+            return $this->loadText();
+        } else {
+            return $this->$name;
+        }
+    }
+
+    private function storeText()
     {
         $addTextArray = ['title' => $this, 'text' => $this->text,
             'author' => $this->author, 'published' => $this->published, 'fileStorage' => $this->fileStorage];
         file_put_contents($this->slug, serialize($addTextArray));
     }
 
-    public function loadText()
+    private function loadText(): string
     {
         if (file_exists($this->slug)) {
             $addTextArray = unserialize(file_get_contents($this->slug));
@@ -32,7 +78,7 @@ class TelegraphText {
             $this->published = $addTextArray['published'];
             $this->fileStorage = $addTextArray['fileStorage'];
         }
-
+        return false;
     }
     public function editText($text, $title){
         $this->text = $text;
--- index.php
+++ index.php
@@ -34,7 +34,7 @@ abstract class View
 
 abstract class User implements EventListenerInterface
 {
-    public $id, $name, $role;
+    protected $id, $name, $role;
 
     abstract public function getTextsToEdit($id, $name, $role): string;
     abstract public function attachEvent(string $className, callable $callback);
@@ -42,14 +42,104 @@ abstract class User implements EventListenerInterface
 }
 
 
-require_once 'FileStorage.php';
+
+class FileStorage extends Storage
+{
+
+
+    public function logMessage(string $error): void
+    {
+        // TODO: Implement logMessage() method.
+    }
+
+
+    public function lastMessages(int $num): array
+    {
+        $numArr = [];
+        return $numArr;
+    }
+
+    public function attachEvent(string $className, callable $callback)
+    {
+        if (is_callable($callback)) {
+            call_user_func('attachEvent', $callback);
+        }
+    }
+
+
+    public function detouchEvent(string $className)
+    {
+        // TODO: Implement detouchEvent() method.
+    }
+
+
+    public function create(TelegraphText $object): string
+    {
+        $fileName = $object->slug . '_' . date('d-m-Y');
+        if (file_exists('file_storage' . '/' . $fileName . '.txt')) {
+            $i = 0;
+            do {
+                $i++;
+                $fileNameConflict = $fileName . '_' . $i;
+            } while (file_exists('file_storage' . '/' . $fileNameConflict . '.txt'));
+            $fileName = $fileNameConflict;
+        }
+        $object->slug = $fileName;
+
+        $data = [
+            'title' => $object->title,
+            'text' => $object->text,
+            'author' => $object->author,
+            'published' => $object->published,
+            'slug' => $object->slug,
+        ];
+
+        file_put_contents('file_storage' . '/' . $fileName . '.txt', serialize($data));
+        return $fileName;
+    }
+
+    public function read(string $slug)
+    {
+        if (file_exists($slug) && filesize($slug) > 0) {
+            $savedData = unserialize(file_get_contents($slug));
+            $post = new TelegraphText($savedData['author'], $savedData['published'],
+                $savedData['slug'], $savedData['fileStorage']);
+            return $post;
+        }
+        return false;
+    }
+
+
+    public function update(string $slug, TelegraphText $data): void
+    {
+        file_put_contents('test_text_file' . '.txt', serialize($data));
+    }
+
+    public function delete(string $slug): void
+    {
+        $file = 'file_storage' . '/' . $slug . '.txt';
+        if (file_exists($file)) {
+            unlink($file);
+        }
+    }
+
+    public function list(): array
+    {
+        $allFiles = array_diff(scandir('file_storage'), array('.', '..'));
+        $result = [];
+        foreach ($allFiles as $file) {
+            $result[$file] = unserialize(file_get_contents('file_storage' . '/' . $file));
+        }
+        return $result;
+    }
+}
 
 
 $telegraphText = new TelegraphText( 'Vasiliy', '19.12.2022',
     'test_text_file', 'C:xampp\htdocs\Telegraph_Project\telegraph\file_storage');
 $fileStorage = new FileStorage();
-$telegraphText->storeText();
-$telegraphText->loadText();
+//$telegraphText->storeText();
+//$telegraphText->loadText();
 $telegraphText->editText( 'Научиться работать с классами и объектами на практике.', 'Практическая работа' );
 echo $telegraphText->text . PHP_EOL;
 
